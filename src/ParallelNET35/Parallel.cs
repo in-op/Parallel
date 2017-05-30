fdaf96e34586356531a422d0084dd57825f87a7c
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-30 02:18:08 -0400

    Fixed bug: state must be shared across all threads

--- src/ParallelNET35/Parallel.cs
+++ src/ParallelNET35/Parallel.cs
@@ -8,11 +8,17 @@ namespace ParallelNET35
 {
     public static class Parallel
     {
-        public static void For(int start, int stop, Action<int> loopBlock)
+        /// <summary>
+        /// Executes a for loop in which iterations may run in parallel.
+        /// </summary>
+        /// <param name="fromInclusive">The start index, inclusive.</param>
+        /// <param name="toExclusive">The end index, exclusive.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        public static void For(int fromInclusive, int toExclusive, Action<int> body)
         {
             int threadCount = Environment.ProcessorCount;
             object locker = new object();
-            int currentIndex = start;
+            int currentIndex = fromInclusive;
             Thread[] threads = new Thread[threadCount];
 
             for (int i = 0; i < threadCount; i++)
@@ -26,7 +32,7 @@ namespace ParallelNET35
                         {
                             lock (locker)
                             {
-                                if (currentIndex >= stop) return;
+                                if (currentIndex >= toExclusive) return;
                                 else
                                 {
                                     threadsCurrentIndex = currentIndex;
@@ -36,7 +42,7 @@ namespace ParallelNET35
                             todo.Invoke(threadsCurrentIndex);
                         }
                     }));
-                threads[i].Start(loopBlock);
+                threads[i].Start(body);
             }
 
             for (int i = 0; i < threadCount; i++)
@@ -44,6 +50,71 @@ namespace ParallelNET35
                 threads[i].Join();
             }
             
+        }
+
+
+
+        public class ParallelLoopState
+        {
+            internal bool stop = false;
+            internal readonly object locker = new object();
+
+            public void Stop()
+            {
+                lock (locker)
+                {
+                    stop = true;
+                }
+            }
+
+            internal ParallelLoopState() { }
+        }
+
+
+        /// <summary>
+        /// Executes a for loop in which iterations may run in parallel.
+        /// </summary>
+        /// <param name="fromInclusive">The start index, inclusive.</param>
+        /// <param name="toExclusive">The end index, exclusive.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        public static void For(int fromInclusive, int toExclusive, Action<int, ParallelLoopState> body)
+        {
+            int threadCount = Environment.ProcessorCount;
+            object locker = new object();
+            int currentIndex = fromInclusive;
+            Thread[] threads = new Thread[threadCount];
+            ParallelLoopState state = new ParallelLoopState();
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i] = new Thread(new ParameterizedThreadStart(
+                    (block) =>
+                    {
+                        Action<int, ParallelLoopState> todo = (Action<int, ParallelLoopState>)block;
+                        int threadsCurrentIndex;
+                        while (true)
+                        {
+                            if (state.stop) return;
+                            lock (locker)
+                            {
+                                if (currentIndex >= toExclusive) return;
+                                else
+                                {
+                                    threadsCurrentIndex = currentIndex;
+                                    currentIndex++;
+                                }
+                            }
+                            todo.Invoke(threadsCurrentIndex, state);
+                        }
+                    }));
+                threads[i].Start(body);
+            }
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i].Join();
+            }
+
         }
     }
 }
---
+++ src/ParallelNET35Tests/ParallelTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelNET35;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelNET35.Tests
+{
+    [TestClass()]
+    public class ParallelTests
+    {
+        [TestMethod()]
+        public void ForCorrectlyFillsArrayWithAValue()
+        {
+            byte comparison = 42;
+            int size = 1000;
+            byte[] bytes = new byte[size];
+            Parallel.For(0, size, (i) =>
+            {
+                bytes[i] = comparison;
+            });
+
+            for (int i = 0; i < size; i++)
+                if (bytes[i] != comparison)
+                    Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ForWithStateCorrectlyFillsArrayWithAValue()
+        {
+            byte comparison = 42;
+            int size = 1000;
+            byte[] bytes = new byte[size];
+            Parallel.For(0, size, (i, state) =>
+            {
+                bytes[i] = comparison;
+            });
+
+            for (int i = 0; i < size; i++)
+                if (bytes[i] != comparison)
+                    Assert.Fail();
+        }
+    }
+}
