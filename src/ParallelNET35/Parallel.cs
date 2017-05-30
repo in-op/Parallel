80970537cf2a44b9c6c67cb0d2fec6e6c234bb11
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-30 03:45:05 -0400

    Added support for Int64 indexing on the Parallel.For loop that holds thread-local data.

--- src/ParallelNET35/Parallel.cs
+++ src/ParallelNET35/Parallel.cs
@@ -221,5 +221,117 @@ namespace ParallelNET35
 
 
 
+        /// <summary>
+        /// Executes a for loop with thread-local data in which iterations may run in parallel, and the state of the loop may be monitored and manipulated.
+        /// </summary>
+        /// <param name="fromInclusive">The start index, inclusive.</param>
+        /// <param name="toExclusive">The end index, exclusive.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        public static void For<TLocal>(
+            int fromInclusive,
+            int toExclusive,
+            Func<TLocal> localInit,
+            Func<int, ParallelLoopState, TLocal, TLocal> body,
+            Action<TLocal> localFinally)
+        {
+            int threadCount = Environment.ProcessorCount;
+            object locker = new object();
+            int currentIndex = fromInclusive;
+            Thread[] threads = new Thread[threadCount];
+            ParallelLoopState state = new ParallelLoopState();
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i] = new Thread(new ParameterizedThreadStart(
+                    (block) =>
+                    {
+                        TLocal localVars = localInit.Invoke();
+                        Func<int, ParallelLoopState, TLocal, TLocal> todo = (Func<int, ParallelLoopState, TLocal, TLocal>)block;
+                        int threadsCurrentIndex;
+                        while (true)
+                        {
+                            if (state.stop) break;
+                            lock (locker)
+                            {
+                                if (currentIndex >= toExclusive) break;
+                                else
+                                {
+                                    threadsCurrentIndex = currentIndex;
+                                    currentIndex++;
+                                }
+                            }
+                            localVars = todo.Invoke(threadsCurrentIndex, state, localVars);
+                        }
+                        localFinally.Invoke(localVars);
+                    }));
+                threads[i].Start(body);
+            }
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i].Join();
+            }
+
+        }
+
+        /// <summary>
+        /// Executes a for loop with thread-local data in which iterations may run in parallel, and the state of the loop may be monitored and manipulated.
+        /// </summary>
+        /// <param name="fromInclusive">The start index, inclusive.</param>
+        /// <param name="toExclusive">The end index, exclusive.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        public static void For<TLocal>(
+            long fromInclusive,
+            long toExclusive,
+            Func<TLocal> localInit,
+            Func<long, ParallelLoopState, TLocal, TLocal> body,
+            Action<TLocal> localFinally)
+        {
+            int threadCount = Environment.ProcessorCount;
+            object locker = new object();
+            long currentIndex = fromInclusive;
+            Thread[] threads = new Thread[threadCount];
+            ParallelLoopState state = new ParallelLoopState();
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i] = new Thread(new ParameterizedThreadStart(
+                    (block) =>
+                    {
+                        TLocal localVars = localInit.Invoke();
+                        Func<long, ParallelLoopState, TLocal, TLocal> todo = (Func<long, ParallelLoopState, TLocal, TLocal>)block;
+                        long threadsCurrentIndex;
+                        while (true)
+                        {
+                            if (state.stop) break;
+                            lock (locker)
+                            {
+                                if (currentIndex >= toExclusive) break;
+                                else
+                                {
+                                    threadsCurrentIndex = currentIndex;
+                                    currentIndex++;
+                                }
+                            }
+                            localVars = todo.Invoke(threadsCurrentIndex, state, localVars);
+                        }
+                        localFinally.Invoke(localVars);
+                    }));
+                threads[i].Start(body);
+            }
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                threads[i].Join();
+            }
+
+        }
+
+
+
+
+
+
+
     }
 }
