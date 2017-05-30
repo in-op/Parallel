9bdb72d9889d9c9581c3dc9afaf5b979f7d38df5
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-29 22:22:39 -0400

    Added little test.

---
+++ src/ParallelNET35/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ParallelNET35
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread t = new Thread(Print);
+            t.Start();
+            for (int j = -20; j < 0; j++)
+                Console.WriteLine(j);
+            t.Join();
+            Console.WriteLine("Threads done");
+            Console.ReadLine();
+        }
+
+        public static void Print()
+        {
+            for (int i = 0; i < 20; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
